9effc8290a29efb9e4d01a4a03a95c5a4530e8ef
czy <pkp789632@gmail>
2016-04-29 14:50:00 +0800

    第一次提交

--- app/src/main/java/com/czy/gitdemo/MainActivity.java
+++ app/src/main/java/com/czy/gitdemo/MainActivity.java
@@ -2,9 +2,12 @@ package com.czy.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
